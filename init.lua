24cbdd529e026e8fe71dc2035a7e812046c41bf1
Bowen Tan <bowentan78@gmail.com>
2024-07-21 20:58:28 +0800

    feat: add rust-tools

--- init.lua
+++ init.lua
@@ -1084,6 +1084,12 @@ require('lazy').setup({
     end,
   },
 
+  {
+    'mrcjkb/rustaceanvim',
+    version = '^4', -- Recommended
+    lazy = false, -- This plugin is already lazy
+  },
+
   -- The following two comments only work if you have downloaded the kickstart repo, not just copy pasted the
   -- init.lua. If you want these files, they are in the repository, so you can just download them and
   -- place them in the correct locations.
