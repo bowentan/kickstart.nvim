e4676ee70d4676b4c1e0784f18a4542356a65ffc
Bowen Tan <bowentan78@gmail.com>
2025-04-11 17:38:01 +0800

    fix: disable colorizer by default

--- lua/custom/plugins/nvim-colorizer.lua
+++ lua/custom/plugins/nvim-colorizer.lua
@@ -5,5 +5,12 @@ return {
     opts = {
       lazy_load = true,
     },
+    config = function()
+      -- Disable by default
+      local colorizer = require 'colorizer'
+      if colorizer.is_buffer_attached() then
+        colorizer.detach_from_buffer()
+      end
+    end,
   },
 }
